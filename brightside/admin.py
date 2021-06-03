0c9976b29dbe9c0a01c54d57d37a9c4463965744
Amani <>
2021-06-03 22:59:47 +0000

    created superuser

--- brightside/admin.py
+++ brightside/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 #from .models import patient
 # Register your models here.
+
+
 #@admin.register(patient)
 #class PollAdmin(admin.ModelAdmin):
  #   list_display = []
