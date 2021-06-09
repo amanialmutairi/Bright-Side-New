a56b9cea961d65f3cb62f0704b05721cd7cb8b6d
Amani <75279870+amanialmutairi@users.noreply.github.com>
2021-06-09 16:53:47 +0000

    minor edits

--- brightside/models.py
+++ brightside/models.py
@@ -9,29 +9,34 @@ class reseptionist(models.Model):
 
   def __str__(self):
         return self.r_username
-#class appointment(models.Model):
+
+#class physician(models.Model):
+
 
 #class service(models.Model):
 class service(models.Model):
   service_name = models.CharField(max_length=50)
   service_price = models.DecimalField(max_digits=8, decimal_places=2)
-  
+  #physician = models.ForeignKey('physician', on_delete=models.CASCADE)
+
   def __str__(self):
         return self.service_name
 
 #class patient(models.Model):
 class patient(models.Model):
     p_username = models.CharField(max_length=50, unique=True, primary_key=True)
-    p_fname = models.CharField(max_length=50)
-    p_lname =  models.CharField(max_length=50)
+    p_first_name = models.CharField(max_length=50)
+    p_last_name =  models.CharField(max_length=50)
     p_email = models.EmailField(max_length= 100, unique=True)
     p_password = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.p_fname
+        return self.p_first_name
 
-#class physician(models.Model):
+
 
 #class bill(models.Model):
 
 #class payment(models.Model):
+
+#class appointment(models.Model):
--- brightside/views.py
+++ brightside/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import patient, reseptionist, service
 # Create your views here.
 
 #reseptionist
