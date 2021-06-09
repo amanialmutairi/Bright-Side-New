f2ee57865fd634d6a75f0b19800400631a875b0c
Amani <75279870+amanialmutairi@users.noreply.github.com>
2021-06-09 16:40:43 +0000

    registered 3 models in admin

--- brightside/admin.py
+++ brightside/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
-#from .models import patient
+from .models import patient, reseptionist, service
 # Register your models here.
 
 
 #@admin.register(patient)
-#class PollAdmin(admin.ModelAdmin):
- #   list_display = []
+@admin.register(patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ['p_username', 'p_fname', 'p_lname', 'p_email', 'p_password']
+
+
+@admin.register(reseptionist)
+class ReseptionistAdmin(admin.ModelAdmin):
+    list_display = ['r_username', 'r_password']
+
+
+@admin.register(service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ['service_name', 'service_price']
