44e44499a87fc0f358e967955a56f23e5450c0ab
Amani <75279870+amanialmutairi@users.noreply.github.com>
2021-06-11 10:25:18 +0000

    added foreign key

--- brightside/admin.py
+++ brightside/admin.py
@@ -1,19 +1,19 @@
 from django.contrib import admin
-from .models import patient, reseptionist, service
+from .models import Patient, Reseptionist, Service
 # Register your models here.
 
 
 #@admin.register(patient)
-@admin.register(patient)
+@admin.register(Patient)
 class PatientAdmin(admin.ModelAdmin):
     list_display = ['p_username', 'p_first_name', 'p_last_name', 'p_email', 'p_password']
 
 
-@admin.register(reseptionist)
+@admin.register(Reseptionist)
 class ReseptionistAdmin(admin.ModelAdmin):
     list_display = ['r_username', 'r_password']
 
 
-@admin.register(service)
+@admin.register(Service)
 class ServiceAdmin(admin.ModelAdmin):
     list_display = ['service_name', 'service_price']
