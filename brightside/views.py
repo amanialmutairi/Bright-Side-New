66b224fec4f218ba3786d585abf866d500322d87
Amani <75279870+amanialmutairi@users.noreply.github.com>
2021-06-14 23:49:08 +0000

    setup bootstrap

--- brightside/views.py
+++ brightside/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from .models import Patient, Reseptionist, Service
 # Create your views here.
-
+def indextest(request):
+  return render(request, 'index.html')
 #reseptionist
 
 #appointment
 
-#room
+#service
 
 
 #patient
