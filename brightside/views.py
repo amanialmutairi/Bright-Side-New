d2db7d206ce3d4311e1feafc06b553ff3938dcdc
Safaa55 <81956328+itssafaa@users.noreply.github.com>
2021-06-17 20:54:43 +0000

    edit

--- brightside/views.py
+++ brightside/views.py
@@ -1,18 +1,5 @@
 from django.shortcuts import render
 from .models import patient, reseptionist, service
-# Create your views here.
 
-#reseptionist
-
-#appointment
-
-#room
-
-
-#patient
-
-#Doctor
-
-#bill
-
-#payment
+def index(request):
+  return render(request, "brightside/index.html")
