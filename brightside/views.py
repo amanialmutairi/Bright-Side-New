946310e577006cd50114c75ed0d6a388a8e3fef7
Amani <75279870+amanialmutairi@users.noreply.github.com>
2021-06-28 11:53:13 +0000

    minor fix

--- brightside/views.py
+++ brightside/views.py
@@ -70,14 +70,14 @@ def index(request):
     appointments = Appointment.objects.filter(appointment_date=date.today()).count()
     patients = Patient.objects.all().count()
     calc_earning = Appointment.objects.all().aggregate(total_earn= Sum('service__service_price'))
-    prices = Appointment.objects.all()
-    
+
+    prices = Appointment.objects.all().order_by("appointment_date")
     revenue = [x.service.service_price for x in prices]
-    
     days = [x.appointment_date for x in prices ]
 
-    
-    c = line_plot(revenue,days)
+    print(f"days: {days}")
+    print(f"rev: {revenue}")
+    c = line_plot(days, revenue)
     
     context = {'daily_appointments': appointments, 'count_patients': patients, 'total': calc_earning,'all_appointments': all_appointments, 'chart': c }
 
